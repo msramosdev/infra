203e74d531f52c9dad4f1b3a3b3ca8d731cbc78f
Miguel Ramos <miguel.santana.ramos@gmail.com>
2019-07-22 22:28:22 -0300

    Adding docker compose

--- docker-installer.sh
+++ docker-installer.sh
@@ -7,3 +7,4 @@ sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-pr
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt-get install docker-ce docker-ce-cli containerd.io
+bash package-installer.sh docker-compose
